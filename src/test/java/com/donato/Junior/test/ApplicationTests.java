d47beced539d8adf239fb42f22b14f9beafe26ee
VictorSDon <victorsimeoni@outlook.com>
2024-06-13 21:46:06 -0300

    initial push

---
+++ src/test/java/com/donato/Junior/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donato.Junior.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
